025bb125eacd73bce7372124263b39a663c6c9c2
Gabor Kevin Barta <gkevinba@gmail.com>
2020-02-27 00:25:51 +0100

    Refactor gulp tasks by adding parameters

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,25 @@ const cleanCSS = require('gulp-clean-css');
 const browserSync = require('browser-sync').create();
 const data = require('gulp-data');
 const fs = require('fs');
+var argv = require('yargs').argv;
 
+const paths = {
+    docs: './docs/',
+    dist: {
+        all: './dist/',
+        html: 'dist/*.html'
+    },
+    pug: {
+        pages: 'src/pages/*.pug',
+        pdf : 'src/pdf/*.pug',
+        all: 'src/**/*.pug'
+    },
+    scss: {
+        main: 'src/assets/styles/main.scss',
+        all: 'src/assets/styles/*.scss'
+    },
+    data: 'src/assets/data.json'
+}
 
 const pugPrettyOptions = {
     doctype: 'html',
@@ -19,81 +37,58 @@ const pugMinifiedOptions = {
 
 const browserSyncOptions = {
     server: {
-        baseDir: './dist/'
+        baseDir: paths.dist.all
     }
 }
 
 function injectJson() {
-    return JSON.parse(fs.readFileSync('src/assets/data.json'))
-} 
+    return JSON.parse(fs.readFileSync(paths.data))
+}
 
-gulp.task('show-args', async function() {
+var pugSource = (argv.pug == undefined) ? null : argv.pug;
+
+// Example for old args, use yargs now
+gulp.task('show-args', async function () {
     console.log(process.argv);
     console.log(process.argv[3]);
 });
 
-gulp.task('build', function() {
-    return gulp.src('/templates/**/*.pug')
-        .pipe(data(injectJson()))
-        .pipe(pug())
-        .pipe(gulp.dest('/dist'));
-});
-
-gulp.task('pug-pages', function () {
-    return gulp.src('src/pages/*.pug')
-        .pipe(data(injectJson()))
-        .pipe(pug(pugPrettyOptions))
-        .pipe(gulp.dest('./dist/'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('pug-pages-build', function () {
-    return gulp.src('src/pages/*.pug')
+gulp.task('pages-build', function () {
+    return gulp.src(paths.pug.pages)
         .pipe(data(injectJson()))
         .pipe(pug(pugMinifiedOptions))
-        .pipe(gulp.dest('./docs/'));
+        .pipe(gulp.dest(paths.docs));
 });
 
-gulp.task('pug-pdf', function () {
-    return gulp.src('src/pdf/*.pug')
+gulp.task('pug', function () {
+    return gulp.src(paths.pug[pugSource])
         .pipe(data(injectJson()))
         .pipe(pug(pugPrettyOptions))
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest(paths.dist.all))
         .pipe(browserSync.stream());
 });
 
-gulp.task('sass', function(){
-    return gulp.src('src/assets/styles/main.scss')
+gulp.task('sass', function () {
+    return gulp.src(paths.scss.main)
         .pipe(sass())
-        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest(paths.dist.all))
         .pipe(browserSync.stream());
 });
 
-gulp.task('sass-build', function(){
-    return gulp.src('src/assets/styles/main.scss')
+gulp.task('sass-build', function () {
+    return gulp.src(paths.scss.main)
         .pipe(sass())
         .pipe(cleanCSS())
-        .pipe(gulp.dest('docs/'))
+        .pipe(gulp.dest(paths.docs))
 });
 
-// Static server
-gulp.task('browser-sync-pages', function () {
+gulp.task('browser-sync', function () {
     browserSync.init(browserSyncOptions);
-    gulp.watch('src/assets/styles/*.scss', gulp.series('sass'));
-    gulp.watch('src/assets/data.json', gulp.series('pug-pages'));
-    gulp.watch('src/**/*.pug', gulp.series('pug-pages'));
-    gulp.watch('dist/*.html').on('change', browserSync.reload);
+    gulp.watch(paths.scss.all, gulp.series('sass'));
+    gulp.watch(paths.data, gulp.series(pugSource));
+    gulp.watch(paths.pug.all, gulp.series(pugSource));
+    gulp.watch(paths.dist.html).on('change', browserSync.reload);
 });
 
-gulp.task('browser-sync-pdf', function () {
-    browserSync.init(browserSyncOptions);
-    gulp.watch('src/assets/styles/*.scss', gulp.series('sass'));
-    gulp.watch('src/assets/data.json', gulp.series('pug-pdf'));
-    gulp.watch('src/**/*.pug', gulp.series('pug-pdf'));
-    gulp.watch('dist/*.html').on('change', browserSync.reload);
-});
-
-
-gulp.task('build', gulp.series('pug-pages-build', 'sass-build'));
-gulp.task('serve-pages', gulp.series('pug-pages', 'sass', 'browser-sync-pages'));
-gulp.task('serve-pdf', gulp.series('pug-pdf', 'sass', 'browser-sync-pdf'));
+gulp.task('build', gulp.series('pages-build', 'sass-build'));
+gulp.task('serve', gulp.series('pug', 'sass', 'browser-sync'));
